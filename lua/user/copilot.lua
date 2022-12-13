249406344c26021a8166d29ba4f99b83b0acdaf5
laudanum <laudanum@laudans.de>
2022-12-13 22:31:17 +0100

    add change to not use copilot on dap repl

--- lua/user/copilot.lua
+++ lua/user/copilot.lua
@@ -2,3 +2,6 @@ vim.cmd [[
   imap <silent><script><expr> <C-A> copilot#Accept("\<CR>")
   let g:copilot_no_tab_map = v:true
   ]]
+vim.api.nvim_set_var("copilot_filetypes", {
+  ["dap-repl"] = false,
+})
